4291a651972b59847769854a8f54980073ef10d7
sor16 <solviro@gmail.com>
2016-07-18 11:33:35 +0000

    adding .keep_all=T to distinct

--- R/validationPlot.R
+++ R/validationPlot.R
@@ -20,7 +20,9 @@ ValidationPlot <- function(cvList,validationMethod,evaluationFunctions=NULL,lege
         validationPlot <- ggplot(data = IBSData,aes(ModelType,IntegratedBrierScore)) + geom_boxplot()
     }else if(validationMethod=="ROC"){
         FullROCData <-  lapply(cvList,bind_rows) %>% bind_rows()
-        summarizedData <- FullROCData %>% group_by(threshold) %>% summarise(TPR=mean(TPR,na.rm=T),FPR=mean(FPR,na.rm=T),AUC=mean(AUC,na.rm=T),cumArea=mean(cumArea,na.rm=T)) %>% ungroup() %>% distinct(TPR,FPR)
+        summarizedData <- FullROCData %>% group_by(threshold) %>%
+            summarise(TPR=mean(TPR,na.rm=T),FPR=mean(FPR,na.rm=T),AUC=mean(AUC,na.rm=T),cumArea=mean(cumArea,na.rm=T)) %>%
+            ungroup() %>% distinct(TPR,FPR,.keep_all=TRUE)
         AUCposition=data.frame(AUC=unique(FullROCData$AUC),x=0.75,y=0.25)
         validationPlot <- ggplot(data=FullROCData,aes(FPR,TPR,label=cumArea)) + geom_path() + geom_abline(slope=1,intercept=0) +
             geom_text(data=AUCposition,aes(x,y,label=paste("AUC=",round(AUC,3)))) + xlim(0,1)+ylim(0,1) + theme_bw() +
@@ -35,9 +37,10 @@ ValidationPlot <- function(cvList,validationMethod,evaluationFunctions=NULL,lege
             legendPosition <- data.frame(x=rep(xlim[1]+0.1,2),y=c(3*ylim[2]/4.,3*ylim[2]/4 - 0.1))
         }
         textDat= data.frame(legendPosition,text=paste(c("A","B"),format(linearModel$coefficients,digits = 2),sep=" = "))
-        validationPlot <- ggplot(data=summarizedData,aes(deciles,proportion)) + geom_point(na.rm=T) + geom_smooth(method = "lm",se=F,na.rm=T) + geom_text(data=textDat,aes(x,y,label=text))+
-            geom_errorbar(aes(ymin = lower,ymax = upper,width=0.01,color="red"),na.rm=T) + theme_bw() + xlim(xlim) + ylim(ylim) +
-            theme(legend.position="none")
+        validationPlot <- ggplot(data=summarizedData,aes(deciles,proportion)) + geom_point(na.rm=T) +
+            geom_smooth(method = "lm",se=F,na.rm=T) + geom_text(data=textDat,aes(x,y,label=text)) +
+            geom_errorbar(aes(ymin = lower,ymax = upper,width=0.01,color="red"),na.rm=T) + theme_bw() +
+            xlim(xlim) + ylim(ylim) +theme(legend.position="none")
     }
     if(animate){
         require(plotly)
