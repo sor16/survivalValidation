2d3d5047675d9efbab8f3f3aa51914e2feb2d237
sor16 <solviro@gmail.com>
2016-07-22 13:10:32 +0000

    typo fix

--- R/evalCox.R
+++ R/evalCox.R
@@ -15,7 +15,7 @@ evalCox <- function(train,test=train,covariates,time,surv=TRUE){
     coxModel <- coxph(as.formula(formula),data=train)
     #Use model to predict outcome in testset
     baseSurvFit <- survfit(coxModel)
-    baseSurv <- approx(baseSurvFit$time,baseSurv$surv,xout=time)$y
+    baseSurv <- approx(baseSurvFit$time,baseSurvFit$surv,xout=time)$y
     LinearPredictor <- predict(coxModel,newdata=test)
     if(surv){
         predictionFUN <- function(i) i^exp(LinearPredictor)
