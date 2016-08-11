d94cdccc5d8e89dda9b22ebed4821b38e8c7f193
sor16 <solviro@gmail.com>
2016-08-11 12:51:42 +0000

    adding summarized_data() to validationPLot, simplifying the function quite a bit

--- R/validationPlot.R
+++ R/validationPlot.R
@@ -13,28 +13,21 @@
 ValidationPlot <- function(cvList,validationMethod,evaluationFunctions=NULL,legendPosition=NULL,animate=FALSE){
     require(ggplot2)
     require(dplyr)
+    summarizedData <- summarize_validation(cvList,validationMethod=validationMethod,simplify=FALSE)
     if(validationMethod=="IBS"){
         if(is.null(evaluationFunctions)) stop("Please insert character vector for which models were used.")
-        IBSData <- sapply(cvList,unlist) %>% rowMeans() %>% data.frame(IntegratedBrierScore = (.))
         ModelType <- factor(evaluationFunctions)
-        validationPlot <- ggplot(data = IBSData,aes(ModelType,IntegratedBrierScore)) + theme_bw() + geom_boxplot()
+        validationPlot <- ggplot(data = summarizedData,aes(ModelType,IntegratedBrierScore)) + theme_bw() + geom_boxplot()
     }else if(validationMethod=="ROC"){
-        FullROCData <-  lapply(cvList,bind_rows) %>% bind_rows()
-        summarizedData <- FullROCData %>% group_by(threshold) %>%
-            summarise(TPR=mean(TPR,na.rm=T),FPR=mean(FPR,na.rm=T),AUC=mean(AUC,na.rm=T),cumArea=mean(cumArea,na.rm=T)) %>%
-            ungroup() %>% distinct(TPR,FPR,.keep_all=TRUE)
         AUCposition=data.frame(AUC=unique(summarizedData$AUC),x=0.75,y=0.25)
         validationPlot <- ggplot(data=summarizedData,aes(FPR,TPR,label=cumArea)) + geom_line() + geom_abline(slope=1,intercept=0) +
             geom_text(data=AUCposition,aes(x,y,label=paste("AUC=",round(AUC,3)))) + xlim(0,1)+ylim(0,1) + theme_bw() +
             ylab("True positive rate") + xlab("False positive rate")
     }else if(validationMethod == "c_statistic"){
         if(is.null(evaluationFunctions)) stop("Please insert character vector for which models were used.")
-        c_statisticData <- cvList %>% sapply(unlist) %>% rowMeans() %>% data.frame(c_statistic = (.))
         ModelType <- factor(evaluationFunctions)
-        validationPlot <- ggplot(data = c_statisticData,aes(ModelType,c_statistic)) + theme_bw() + geom_boxplot()
+        validationPlot <- ggplot(data = summarizedData,aes(ModelType,c_statistic)) + theme_bw() + geom_boxplot()
     }else if(validationMethod=="calibration"){
-        fullCalibrationData <-  lapply(cvList,bind_rows) %>% bind_rows()
-        summarizedData <- fullCalibrationData %>% group_by(deciles) %>% summarise(proportion=mean(proportion,na.rm=T),lower=mean(lower,na.rm=T),upper=mean(upper,na.rm=T))
         linearModel=with(summarizedData,lm(proportion~deciles))
         xlim <- with(summarizedData,range(deciles[!is.na(proportion)])) + c(-0.05,0.05)
         ylim <- with(summarizedData,c(min(lower,na.rm=T),max(upper,na.rm=T)))
