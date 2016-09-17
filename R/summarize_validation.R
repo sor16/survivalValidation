2ca8cbae3c7ff73bd8e55fdeb3e49d10bf33c1a0
sor16 <solviro@gmail.com>
2016-09-17 23:15:23 +0000

    Some changes related to calibration lm fit

--- R/summarize_validation.R
+++ R/summarize_validation.R
@@ -13,10 +13,11 @@ summarize_validation <- function(cvList,validationMethod,simplify=FALSE){
             ungroup() %>% distinct(TPR,FPR,.keep_all=TRUE)
         if(simplify) return(summarizedData %>% distinct(AUC))
     }else if(validationMethod=="calibration"){
-        summarizedData <- lapply(cvList,bind_rows) %>% bind_rows() %>% group_by(deciles) %>%
-            summarise(Proportion=median(proportion,na.rm=T),lower=quantile(proportion,probs=0.025,na.rm=T),upper=quantile(proportion,probs=0.975,na.rm=T))
+        lmData <- lapply(cvList,bind_rows) %>% bind_rows() %>% group_by(deciles)
+        linearModel <- with(summarizedData,lm(proportion~deciles))
+        summarizedData <- lmData %>% summarise(Proportion=median(proportion,na.rm=T),lower=quantile(proportion,probs=0.025,na.rm=T),upper=quantile(proportion,probs=0.975,na.rm=T))
         #summarise(proportion=mean(proportion,na.rm=T),lower=mean(lower,na.rm=T),upper=mean(upper,na.rm=T))
-        linearModel=with(summarizedData,lm(Proportion~deciles))
+
         if(simplify) return(data.frame(A = linearModel$coefficients[[1]], B = linearModel$coefficients[[2]], MSE=mean(linearModel$residuals^2)))
     }else if(validationMethod=="c_statistic"){
         summarizedData <- cvList %>% sapply(unlist) %>% rowMeans() %>% data.frame(c_statistic = (.))
